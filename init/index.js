72f17b886e76e5f52d7ae89ab18c7fae8a9adcff
harshal2390 <harshalchaudhari534@gmail.com>
2025-06-26 18:02:09 +0530

    Add root redirect to /listings

--- init/index.js
+++ init/index.js
@@ -1,6 +1,10 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const initData = require("./data.js");
+const dbUrl = process.env.ATLASDB_URL;
+console.log("Connecting to:", dbUrl);
 main()
   .then(() => {
     console.log("connect to DB");
@@ -9,14 +13,18 @@ main()
     console.log("error in database connection");
   });
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+  try {
+    await mongoose.connect(dbUrl);
+  } catch (err) {
+    console.error("MongoDB connection failed:", err.message);
+  }
 }
 
 const initDB = async () => {
   await Listing.deleteMany({});
   initData.data = initData.data.map((obj) => ({
     ...obj,
-    owner: "6858d47a549f1436bb385538",
+    owner: "685d2c66fa036cb99649453c",
   }));
   await Listing.insertMany(initData.data);
   console.log("data was intilazied");
